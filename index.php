d9fc1cfd7fc3246038a947ec966b30ed6df55229
lutfur <39017937+lutfur06@users.noreply.github.com>
2024-08-14 08:53:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,37 +16,39 @@ public array $orderData = [];
 
     function addTitle(string $title):string{
         $this->title = $title;
-        return "{$this->title} \n";
+        $this->orderData['title'] = $title;
+        return "{$this->title}";
     }
     function addItem(string $item, int $price): string{
         $newArray = [];
         $this->item = $item;
         $this->price = $price;
         array_push($newArray, $this->item,$this->price);
-        $this->orderData[] = $newArray;
-        return "{$this->item} - {$this->price} \n";
+        $this->orderData['order'][] = $newArray;
+        return "{$this->item} - {$this->price}";
     }
 
      function addTotal():string{
         $total = 0;
         $total += $this->price;
         $this->total = $total;
-         return "Total: {$total} \n";
+        $this->orderData['total'] = $total;
+         return "Total: {$total}";
 }
     function createInvoice():?string{
-        $titleOfInvoice = "{$this->title} \n";
+        $titleOfInvoice = "{$this->orderData['title']} \n";
         $file = fopen("data.txt","w");
         fwrite($file,$titleOfInvoice);
 
 
-foreach ($this->orderData as $item){
+foreach ($this->orderData['order'] as $item){
     $data = "{$item[0]} - {$item[1]} \n";
     fwrite($file,$data);
     //file_put_contents("data.txt", $data);
 };
 $dashedLine = "--------------\n";
         fwrite($file,$dashedLine);
-        $totalPrice = "Total  = {$this->total}";
+        $totalPrice = "Total  = {$this->orderData['total']}\n";
         fwrite($file,$totalPrice);
         echo "Done";
         fclose($file);
@@ -61,7 +63,6 @@ $inv->addTitle("My First Invoice");
 $inv->addItem("Item 2", 200);
 $inv->addItem("Item 3", 300);
 $inv->addTotal();
-var_dump($inv->orderData) ;
-var_dump($inv->title) ;
+//`var_dump($inv->orderData) ;
+//var_dump($inv->title) ;
 $inv->createInvoice();
-
